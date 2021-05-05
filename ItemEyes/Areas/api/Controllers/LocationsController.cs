c81ac81260890b9c8c08cd2f0531a53e9106c14f
Kellen Sullivan <k3ll3n@live.com>
2021-05-05 16:27:11 -0500

    Adding project to repo

---
+++ ItemEyes/Areas/api/Controllers/LocationsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ItemEyes.Data;
+using ItemEyes.Models;
+
+namespace ItemEyes.Areas.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ItemContext _context;
+
+        public LocationsController(ItemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Locations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Location>>> GetLocations()
+        {
+            var locations = await _context.Locations
+                .Include(l => l.Items)
+                .ToListAsync();
+
+            return locations;
+        }
+
+        // GET: api/Locations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Location>> GetLocation(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return location;
+        }
+
+        // POST: api/Locations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Location>> PostLocation(Location location)
+        {
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocation", new { id = location.Id }, location);
+        }
+
+        // DELETE: api/Locations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLocation(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ItemEyes/Data/ItemContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ItemEyes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemEyes.Data
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().Ignore(i => i.Location);
+            modelBuilder.Entity<Location>().Ignore(l => l.Items);
+
+            modelBuilder.Entity<Item>().ToTable("Item");
+            modelBuilder.Entity<Location>().ToTable("Location");
+
+            modelBuilder.Entity<Item>()
+                .HasOne(i => i.Location)
+                .WithMany(l => l.Items);
+
+            modelBuilder.Entity<Item>()
+                .Property(i => i.Id)
+                .UseIdentityColumn(seed: 1, increment: 1);
+
+            modelBuilder.Entity<Item>()
+                .Property(i => i.LocationId)
+                .HasDefaultValue(1);
+
+        }
+    }
+}
