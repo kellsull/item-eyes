a6f69bdcfb80611098b503d1220848394cde87c5
Kellen Sullivan <k3ll3n@live.com>
2021-05-06 10:59:32 -0500

    Update template

--- ItemEyes/Controllers/HomeController.cs
+++ ItemEyes/Controllers/HomeController.cs
@@ -1,25 +1,32 @@
-﻿using ItemEyes.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ItemEyes.Data;
+using ItemEyes.Models;
 
 namespace ItemEyes.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ItemContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ItemContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var locationContext = await _context.Locations
+                .Include(l => l.Items)
+                .ToListAsync();
             return View();
         }
 
